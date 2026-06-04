acb36a316331398e8abd3687ad5bc766b014fb26
lucas@zacharias-net.de <lucas@zacharias-net.de>
2026-06-04 17:14:00 +0200

    asas

--- resources/views/livewire/admin/user-profile.blade.php
+++ resources/views/livewire/admin/user-profile.blade.php
@@ -27,34 +27,7 @@
         <h2 class="font-semibold text-lg">{{ $user->name }}</h2>
     </div>
 
-    <!-- Benutzerstatistiken -->
-    <ul class="py-4 mt-2 text-gray-700 flex items-center justify-around">
-        <li class="flex flex-col items-center justify-around flex-1">
-            <svg class="w-6 h-6 fill-current text-blue-900" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                <path d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z" />
-            </svg>
-            <div class="text-center">{{ $user->followers()->count() }} Follower</div>
-        </li>
-        <li class="flex flex-col items-center justify-around flex-1">
-            <svg class="w-6 h-6 fill-current text-blue-900" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                <path d="M7 8a4 4 0 1 1 0-8 4 4 0 0 1 0 8zm0 1c2.15 0 4.2.4 6.1 1.09L12 16h-1.25L10 20H4l-.75-4H2L.9 10.09A17.93 17.93 0 0 1 7 9zm8.31.17c1.32.18 2.59.48 3.8.92L18 16h-1.25L16 20h-3.96l.37-2h1.25l1.65-8.83zM13 0a4 4 0 1 1-1.33 7.76 5.96 5.96 0 0 0 0-7.52C12.1.1 12.53 0 13 0z" />
-            </svg>
-            <div class="text-center">{{ $user->followedCustomers()->count() }} gefolgt</div>
-        </li>
-        <li class="flex flex-col items-center justify-around flex-1">
-            <svg class="w-6 h-6 fill-current text-blue-900" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                <path d="M9 12H1v6a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-6h-8v2H9v-2zm0-1H0V5c0-1.1.9-2 2-2h4V2a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v1h4a2 2 0 0 1 2 2v6h-9V9H9v2zm3-8V2H8v1h4z" />
-            </svg>
-            <div class="text-center">{{ $user->likedProducts()->count() }} Produkte geliked</div>
-        </li>
-        <li class="flex flex-col items-center justify-around flex-1">
-            <svg class="w-6 h-6  text-blue-900" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 10h9.231M6 14h9.231M18 5.086A5.95 5.95 0 0 0 14.615 4c-3.738 0-6.769 3.582-6.769 8s3.031 8 6.769 8A5.94 5.94 0 0 0 18 18.916"/>
-            </svg>
-
-            <div class="text-center">{{ number_format($user->customer->earnings() ?? 0, 2) }} € Einkünfte</div>
-        </li>
-    </ul>
+    
 
 
     <!-- Weitere Aktionen -->
@@ -114,16 +87,6 @@
             </button>
         </li>
         
-        <!-- Buchungen Tab -->
-        <li class="w-full border-l border-gray-200">
-            <button 
-                @click="selectedTab = 'shelfRentals'" 
-                :class="{ 'text-blue-600 bg-white border-b-2 border-blue-600': selectedTab === 'shelfRentals' }" 
-                class="w-full p-4 transition-all duration-200 bg-gray-100 hover:bg-blue-100 hover:text-blue-600 focus:outline-none"
-            >
-                Buchungen
-            </button>
-        </li>
 
     </ul>
 
@@ -187,32 +150,6 @@
                 @else
                     <p class="text-gray-500">Keine Kundendetails verfügbar.</p>
                 @endif
-            </div>
-        </div>
-        <div  x-show="selectedTab === 'shelfRentals'" x-collapse  x-cloak>
-            <div class="w-full bg-gray-100 shadow rounded-lg p-6 mt-4">
-                <h2 class="text-2xl font-bold text-gray-800 mb-4">Buchungen</h2>
-                <livewire:admin.user-profile.shelf-rentals :user-id="$user->id" x-collapse  lazy />
-                <div wire:loading>
-                    <div class="flex justify-center items-center h-20">
-                        <svg class="animate-spin h-5 w-5 text-gray-700" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-                            <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v8H4z"></path>
-                        </svg>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div  x-show="selectedTab === 'logs'" x-collapse  x-cloak>
-            <div class="w-full bg-gray-100 shadow rounded-lg p-6 mt-4">
-                <h2 class="text-2xl font-bold text-gray-800 mb-4">Aktivitäten</h2>
-
-            </div>
-        </div>
-        <div  x-show="selectedTab === 'payouts'" x-collapse  x-cloak>
-            <div class="w-full bg-gray-100 shadow rounded-lg p-6 mt-4">
-                <h2 class="text-2xl font-bold text-gray-800 mb-4">Auszahlungen</h2>
-
             </div>
         </div>
     </div>
