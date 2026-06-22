f0deaa06788fac91559b2c7b34923349b9911341
lucas@zacharias-net.de <lucas@zacharias-net.de>
2026-06-22 04:30:46 +0200

    fsfd

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -48,12 +48,6 @@
                         <span data-key="t-safety">Aktivitäten</span>
                     </a>
                 </li>
-                <li>
-                    <a href="{{ route('admin.exports') }}"   class="block py-2.5 px-6 text-sm font-medium text-gray-600 transition-all duration-150 ease-linear hover:text-blue-500 ">
-                        <i data-feather="sliders" fill="#545a6d33"></i>
-                        <span data-key="t-exports">Exporte</span>
-                    </a>
-                </li>
                 <li class="px-5 py-3 text-xs font-medium text-gray-500 cursor-default leading-[18px] group-data-[sidebar-size=sm]:hidden block" data-key="t-menu">Management</li>
 
                 <li>
